889000e9eb70579d9e55ee3b96b7aa1941422ee3
Superkooka <35631093+Superkooka@users.noreply.github.com>
2020-09-27 19:18:59 +0200

    Rename variable

--- src/Service/DownloaderService.php
+++ src/Service/DownloaderService.php
@@ -78,7 +78,7 @@ class DownloaderService
 
             $chaptersCounter = 0;
 
-            foreach ($urls as $k => $url){
+            foreach ($urls as $name => $url){
                 if(preg_match("/\/activity\/[0-9]{3}$/", $url)) {
                     unset($urls[$k]);
                 }
